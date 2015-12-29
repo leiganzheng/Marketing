30489cb4e6af8b584d4c37b3250ed8080bb1d161
leiganzheng <qoocc03@qoocc03deiMac.local>
2015-12-29 17:48:46 +0800

    添加类

---
+++ Marketing/Marketing/UserCenter/OrderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderTableViewCell.swift
+//  QooccDoctor
+//
+//  Created by leiganzheng on 15/12/29.
+//  Copyright © 2015年 juxi. All rights reserved.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Marketing/Marketing/UserCenter/PayOrderViewController.swift
+++ Marketing/Marketing/UserCenter/PayOrderViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ChangePWDViewController.swift
+//  PayOrderViewController.swift
 //  Marketing
 //
 //  Created by leiganzheng on 15/12/29.
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class ChangePWDViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+class PayOrderViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
     
     var titles: NSArray!
     @IBOutlet weak var customTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "修改密码"
-        self.titles = [["请输入当前密码"],["请输入您的新密码","请再次输入您的新密码"],["修改密码"]]
+        self.title = "订单支付"
+        self.titles = ["微信支付","快捷支付"]
         // Do any additional setup after loading the view.
     }
     
@@ -26,36 +26,37 @@ class ChangePWDViewController: UIViewController , UITableViewDataSource, UITable
     
     // MARK: UITableViewDataSource, UITableViewDelegate
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titles[section].count
-    }
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return titles.count
     }
+    
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 44
     }
     func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 10.0*COEFFICIENT_OF_HEIGHT_ZOOM
+        return 30
     }
-    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let lb = UILabel(frame: CGRectMake(8,7,200, 30))
+        lb.backgroundColor = UIColor.clearColor()
+        lb.textColor = tableViewCellDefaultTextColor
+        lb.textAlignment = NSTextAlignment.Left
+        lb.font = UIFont.systemFontOfSize(15)
+        lb.text = "请支付"
+        return lb
+    }
     func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 0.01*COEFFICIENT_OF_HEIGHT_ZOOM
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            let cellId = "cell"
-            var cell: UITableViewCell! = self.customTableView.dequeueReusableCellWithIdentifier(cellId)
-            if cell == nil {
-                cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellId)
-                let lb = UILabel(frame: CGRectMake(8,7,200, 30))
-                lb.backgroundColor = UIColor.clearColor()
-                lb.textColor = tableViewCellDefaultTextColor
-                lb.textAlignment = NSTextAlignment.Left
-                lb.font = UIFont.systemFontOfSize(15)
-                let titleArray = self.titles[indexPath.section] as! NSArray
-                lb.text = titleArray[indexPath.row] as? String
-                cell.addSubview(lb)
-            }
-            return cell
+        let cellId = "cell1"
+        var cell: UITableViewCell! = self.customTableView.dequeueReusableCellWithIdentifier(cellId)
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellId)
+            cell.accessoryType = .DisclosureIndicator
+        }
+        cell.textLabel?.text = self.titles[indexPath.row] as? String
+//        cell.imageView?.image = UIImage(named: (iconsArray[indexPath.row] as? String)!)
+        return cell
     }
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
